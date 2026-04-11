5daed7ed46671eb90961e61c5940f74ffb4665ee
cardozo-elias <cardozoagustin847@gmail.com>
2026-04-11 15:04:22 -0300

    algo

--- app/playlists/[id]/PlaylistDetailClient.jsx
+++ app/playlists/[id]/PlaylistDetailClient.jsx
@@ -2,7 +2,7 @@
 import React, { useState, useEffect, useMemo } from 'react';
 import { io } from "socket.io-client";
 import { DragDropContext, Droppable, Draggable } from '@hello-pangea/dnd';
-import { useLanguage } from "../../components/LanguageContext";
+import { useLanguage } from '../../../components/LanguageContext';
 
 // --- ÍCONOS SVG MINIMALISTAS ---
 const MusicNoteIcon = () => <svg className="w-12 h-12 text-gray-600 opacity-50" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="1.5"><path strokeLinecap="round" strokeLinejoin="round" d="M9 19V6l12-3v13M9 19c0 1.105-1.343 2-3 2s-3-.895-3-2 1.343-2 3-2 3 .895 3 2zm12-3c0 1.105-1.343 2-3 2s-3-.895-3-2 1.343-2 3-2 3 .895 3 2zM9 10l12-3" /></svg>;
